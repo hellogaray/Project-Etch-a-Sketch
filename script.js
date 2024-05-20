08839a56584feba1dff6cc24848c75f33f7883e2
Leo <137473678+hellogaray@users.noreply.github.com>
2024-05-20 22:12:55 +0900

    Set Draw/Rainbow/Erase/Clear functionality

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 const container = document.getElementById("container");
+const sideBar = document.getElementById("sideBar");
+var action = "d";
 
+// Check which action to do
+sideBar.addEventListener("click", function(event) {
+    const target = event.target;
+    if (target.matches("#rainbowBtn")) {
+        action = "r";
+    } else if (target.matches("#eraseBtn")) {
+        action = "e";
+    } else {
+        action = "d"
+    }
+});
+
+// Clears the grid
+document.getElementById("clearBtn").onclick = function() {
+    gridCleaner()
+    gridMaker(16, 16);
+}
+
+// Creates the grid
 function gridMaker(rows, cols) {
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
@@ -10,15 +31,17 @@ function gridMaker(rows, cols) {
 
         for (var j = 0; j < cols; j++) {
             const cell = document.createElement('div');
-            cell.textContent = "";
             cell.classList.add("cell" + j);
-
-            cell.addEventListener("mouseover", function() {
-                paintOver(cell);
-            });
-
-            cell.addEventListener("mousedown", function() {
-                clearPaint(cell);
+            cell.addEventListener("mousemove", function(event) {
+                if (event.buttons === 1) {
+                    if (action === "d") {
+                        paintOver(cell);
+                    } else if (action === "r") {
+                        rainbowOver(cell);
+                    } else if (action === "e") {
+                        clearPaint(cell);
+                    }
+                }
             });
 
             col.appendChild(cell);
@@ -27,14 +50,25 @@ function gridMaker(rows, cols) {
     }
 }
 
-// Mouse Over Function to Paint
+//Sets cell's background to black
 function paintOver(cell) {
     cell.style.backgroundColor = "black";
 }
 
-// On Click Function to Clear Paint
+//Sets cell's background to white (clear)
 function clearPaint(cell) {
     cell.style.backgroundColor = "";
+}
+
+//Sets cell's background to a random hex color
+function rainbowOver(cell) {
+    const randomHexColor = '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16).padStart(6, '0');
+    cell.style.backgroundColor = randomHexColor
+}
+
+// Cleans the grid
+function gridCleaner() {
+    container.innerHTML = ''
 }
 
 gridMaker(16, 16);
